e819c3052373f3772ec5c6b536ad9faf4734e1c4
ouzuyu <oupenguin@gmail.com>
2019-08-19 00:20:21 +0800

    style: 风格调整

--- template/route.js
+++ template/route.js
@@ -1,35 +1,35 @@
-import breadcrumbs from "@/components/breadcrumb/breadcrumb.config.json"
+import breadcrumbs from '@/components/breadcrumb/breadcrumb.config.json'
 
 export default ({ app, store }) => {
   app.router.beforeEach((to, from, next) => {
     const paths = to.path
-      .split("/")
+      .split('/')
       .filter(p => p)
-      .map(p => "/" + p)
+      .map(p => '/' + p)
       .map((path, index, paths) => {
 
         // 第 0 个不需拼接
         if (index) {
-          let result = "";
+          let result = ''
           for (let i = 0; i <= index; i++) {
-            result += paths[i];
+            result += paths[i]
           }
-          return result;
+          return result
         }
-        return path;
+        return path
       })
       .map(path => {
-        const item = breadcrumbs.find(bread => bread.path === path);
+        const item = breadcrumbs.find(bread => bread.path === path)
         if (item) {
-          return item;
+          return item
         }
         return {
-          name: path.split("/").pop(),
+          name: path.split('/').pop(),
           path,
           clickable: false,
           isShow: true
-        };
-      });
+        }
+      })
 
     store.commit('breadcrumb/setBreadcrumb', paths)
     next()
