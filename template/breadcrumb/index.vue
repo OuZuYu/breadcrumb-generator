3398b6c8e0425c0958ff15e51232d5aba4ada272
ouzuyu <oupenguin@gmail.com>
2019-08-18 20:39:31 +0800

    init

---
+++ template/breadcrumb/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="isBreadcrumbShow">
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item
+        v-for="(item, index) in breadcrumbData"
+        :to="item.clickable ? item.path : ''"
+        :key="index">
+        {{ item.name }}
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+import breadcrumbs from "./breadcrumb.config"
+
+export default {
+  name: 'Breadcrumb',
+
+  computed: {
+    isBreadcrumbShow () {
+      return this.breadcrumbData && this.breadcrumbData.length && this.curBreadcrumb.isShow
+    },
+
+    breadcrumbData () {
+      return this.$store.state.breadcrumb.breadcrumbData
+    },
+
+    curBreadcrumb () {
+      return this.breadcrumbData.find(breadcrumb => breadcrumb.path === this.$route.path) || {}
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
